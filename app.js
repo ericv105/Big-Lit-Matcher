33068be66d0772f318b02f222b6f935ecb9d04eb
ericv105 <ericvelepucha@gmail.com>
2022-08-26 01:51:48 -0400

    deployed on heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const util = require('./services/MatrixFormParser')
 const express = require("express");
+const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
-const port = 3001;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -16,6 +18,13 @@ app.post("/api", (req, res) => {
   // prefs object multipart => stable matching algorithm => stable matches
   res.json(util.getAllStableMatchings(prefsObject));
 });
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
